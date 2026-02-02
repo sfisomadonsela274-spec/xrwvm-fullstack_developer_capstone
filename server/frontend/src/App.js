265ef274b29fc73709ae040660e061067a40a4c1
sfisomadonsela274-spec <sfisomadonsela274@gmail.com>
2026-02-02 11:10:43 -0500

    Add login/logout views and register page

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,17 @@
-import LoginPanel from "./components/Login/Login"
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      {/* Add a default route if needed */}
+      <Route path="*" element={<div>Please go to /login or /register</div>} />
     </Routes>
   );
 }
+
 export default App;
